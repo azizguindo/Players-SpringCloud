51d19dd40631596b33e19809aff4689a336cf4c2
Aziz <abazizguido@gmail.com>
2020-12-09 16:28:27 +0100

    Initial commit

---
+++ Playersmsjpa/src/main/java/master/microservices/playersmsjpa/PlayermsjpaApplication.java
@@ -0,0 +1,20 @@
+package master.microservices.playersmsjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class PlayermsjpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlayermsjpaApplication.class, args);
+    }
+
+    @Bean
+    public InitBD schedulerRunner() {
+        return new InitBD();
+    }
+}
